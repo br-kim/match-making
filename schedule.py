2e07ff9a4711cc3789f26dca265acfedd29227cb
br-kim <idle947@gmail.com>
2023-09-23 20:18:25 +0900

    2vs2 매칭 로직 수정

--- schedule.py
+++ schedule.py
@@ -52,17 +52,19 @@ def game_matching_2vs2():
     """
     db = next(get_db())
     match_making_info_list = crud.get_match_making_by_game_type(db=db, game_type="2vs2")
+    matched_user_id_set = set()
     if len(match_making_info_list) >= 4:
         idx = 0
         while idx < len(match_making_info_list) - 3:
             info1, info2 = match_making_info_list[idx], match_making_info_list[idx + 3]
             room_id = str(uuid.uuid4())
+            matching_user_list = [
+                info.user for info in match_making_info_list[idx : idx + 4]
+            ]
             user_id_list = [
                 info.user_id for info in match_making_info_list[idx : idx + 4]
             ]
-            matching_correction = calculate_2vs2_matching_correction(
-                [info.user for info in match_making_info_list[idx : idx + 4]]
-            )
+            matching_correction = calculate_2vs2_matching_correction(matching_user_list)
             if info2.user.mmr - info1.user.mmr < matching_correction:
                 for user_id, team in zip(user_id_list, ["A", "B", "B", "A"]):
                     crud.update_room_id_by_user_id(
@@ -73,10 +75,16 @@ def game_matching_2vs2():
                         team=team,
                     )
                 print(user_id_list, "매칭 성공")
+                for user in matching_user_list:
+                    matched_user_id_set.add(user.user_id)
+                    user.matching_factor.waiting = 0
                 idx += 1
             idx += 1
     else:
         print("유저 수 부족. 2vs2 매칭 실패")
+    for info in match_making_info_list:
+        if info.user_id not in matched_user_id_set:
+            info.user.matching_factor.waiting += 1
 
 
 def calculate_1vs1_matching_correction(user1: models.User):
@@ -99,5 +107,5 @@ def calculate_2vs2_matching_correction(users: List[models.User]):
     default_correction = 100
     result = 0
     for user in users:
-        result += user.matching_factor.waiting
+        result += min(10, user.matching_factor.waiting)
     return default_correction + (result // len(users))
